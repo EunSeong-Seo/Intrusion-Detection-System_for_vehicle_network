90d4ac309f4a9598f2880e23505ebede00af2c2d
EunSeong <seoun4612@gmail.com>
2022-08-17 20:31:15 +0900

    modify some parts

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,25 @@
-You can download dataset below site
-https://ieee-dataport.org/open-access/car-hacking-attack-defense-challenge-2020-dataset#files
 
+
+
+# IDS for vehicle network(Controller Area Network)
+
+This is IDS for vehicle network.
+
+0. You can download dataset below site
+    https://ieee-dataport.org/open-access/car-hacking-attack-defense-challenge-2020-dataset#files
+1. CAN data EDA - data_EDA.ipnb
+2. Preprocess for apply machine learning models - preprocessing.py
+3. Models
+- Logistic Regression [Done]
+- RandomForest [Done]
+=======Preparing=======
+- Decision Trees
+- Naive Bayes
+- KNN
+- SVM
+- ANN(2 hidden layers)
+- LSTM(256 hidden units)
+- CNN
+
+
+Thank you.
--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,28 @@
 import pandas as pd
 
+def change_hex_to_int(x):
+    if x is None:
+        return int(-1)
+    return int(x, 16)
+
 
 def make_train_xy_df(path):
     # Load data from path
     df = pd.read_csv(path)
     features = ['Timestamp', 'Arbitration_ID', 'DLC', 'Data', 'Class', 'SubClass']
+
+    # Change hex to int and fill -1 at NaN data
+    df['Arbitration_ID'] = df['Arbitration_ID'].apply(change_hex_to_int)
     data_x = df.loc[:, ['Timestamp', 'Arbitration_ID']]
-    data_y = df.loc[:, ['SubClass']] # Class or SubClass??
-    pd.get_dummies(data_y)
+    data_y = df.loc[:, ['SubClass']]
+    '''
+    mapping_dict = {'Normal': 1,
+                    'Flooding': 2,
+                    'Spoofing': 3,
+                    'Replay': 4,
+                    'Fuzzing': 5, }
+    data_y = data_y['SubClass'].apply(lambda x : mapping_dict)
+    '''
     return data_x, data_y
 
 
@@ -17,7 +32,22 @@ def make_train_xy_df_with_D(path):
     features = ['Timestamp', 'Arbitration_ID', 'DLC', 'Data', 'Class', 'SubClass']
     df_data = df['Data'].str.split(' ', expand=True)
     df_data.columns = ['D0', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7']
-    df_data
-    # I have to modify D0 ~ D7 to binaryencoding
-    data_x = df.loc[:, ['Timestamp', 'Arbitration_ID', 'DLC', 'Data']]
+
+    # concat Arbitraion ID, D0 ~ D7 columns after Changing hex to int or fill -1 at NaN data
+    df['Arbitration_ID'] = df['Arbitration_ID'].apply(change_hex_to_int)
+    df_data['D0'] = df_data['D0'].apply(change_hex_to_int)
+    df_data['D1'] = df_data['D1'].apply(change_hex_to_int)
+    df_data['D2'] = df_data['D2'].apply(change_hex_to_int)
+    df_data['D3'] = df_data['D3'].apply(change_hex_to_int)
+    df_data['D4'] = df_data['D4'].apply(change_hex_to_int)
+    df_data['D5'] = df_data['D5'].apply(change_hex_to_int)
+    df_data['D6'] = df_data['D6'].apply(change_hex_to_int)
+    df_data['D7'] = df_data['D7'].apply(change_hex_to_int)
+    data_x = pd.concat([df.loc[:, ['Timestamp', 'Arbitration_ID', 'DLC']], df_data], axis=1)
     data_y = df.loc[:, ['SubClass']]
+    return data_x, data_y
+
+
+if __name__ == '__main__':
+    import pandas as pd
+    path = 'dataset/Pre_train_D_attack_1.csv'
